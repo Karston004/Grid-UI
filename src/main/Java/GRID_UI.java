8ee5f4a18c77abe8117700ec80492589e9b0f22a
Joe Smith <004jsmith@gmail.com>
2025-06-13 06:45:41 +0100

    Feat: Created Folders

---
+++ src/main/Java/GRID_UI.java
@@ -0,0 +1,14 @@
+package src.main.Java;
+//This is the main class used to start the system and initalize the other classes
+
+import javax.swing.JFrame;
+
+public class GRID_UI {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(1080,720);
+        frame.setVisible(true);
+
+    }
+}
