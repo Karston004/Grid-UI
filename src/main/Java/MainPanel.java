f5f3317ceebd444d30eae63296e4e999aa16b53c
Joe Smith <004jsmith@gmail.com>
2025-06-13 10:49:21 +0100

    chore: organised folders

--- src/main/Java/MainPanel.java
+++ src/main/Java/MainPanel.java
@@ -2,13 +2,15 @@ package src.main.java;
 
 import javax.swing.JPanel;
 
+import src.main.java.gridCanvas.SqaureGridCanvas;
+
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 
 public class MainPanel extends JPanel {
-    GridCanvas mainCanvas;
+    SqaureGridCanvas mainCanvas;
     MainPanel () {
         super(new GridBagLayout());
         setBorder(BorderFactory.createLineBorder(Color.RED));
@@ -17,7 +19,7 @@ public class MainPanel extends JPanel {
     }
 
     void addMainCanvas(int gridx, int gridy){
-        mainCanvas = new GridCanvas();
+        mainCanvas = new SqaureGridCanvas();
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1;
