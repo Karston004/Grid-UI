80a259491f146a28eccfec2481a4ee6927b59d6c
Joe Smith <004jsmith@gmail.com>
2025-06-13 11:02:24 +0100

    Feat: updated MainPanel for abstractClass

--- src/main/Java/MainPanel.java
+++ src/main/Java/MainPanel.java
@@ -2,24 +2,31 @@ package src.main.java;
 
 import javax.swing.JPanel;
 
+import src.main.java.gridCanvas.AbstractGridCanvas;
 import src.main.java.gridCanvas.SqaureGridCanvas;
 
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.HashMap;
+
 import javax.swing.BorderFactory;
 
 public class MainPanel extends JPanel {
-    SqaureGridCanvas mainCanvas;
+    AbstractGridCanvas mainCanvas;
     MainPanel () {
         super(new GridBagLayout());
         setBorder(BorderFactory.createLineBorder(Color.RED));
 
-        addMainCanvas(0, 0);
+
+        HashMap<Integer, Color> defaultColorMap = new HashMap<Integer,Color>();
+        defaultColorMap.put(0, Color.GRAY);
+        defaultColorMap.put(1, Color.BLACK);
+        addMainCanvas(0, 0, defaultColorMap);
     }
 
-    void addMainCanvas(int gridx, int gridy){
-        mainCanvas = new SqaureGridCanvas();
+    void addMainCanvas(int gridx, int gridy, HashMap<Integer, Color> colorMap){
+        mainCanvas = new SqaureGridCanvas(gridx, gridy, colorMap);
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1;
