b2c8c10746f43f954827a34e9d0c15c832c2dbdb
Smithj50 <j.smith47@lancaster.ac.uk>
2025-06-17 08:02:35 +0100

    Feat: Updated example buttons color

--- src/main/Java/ToolBar.java
+++ src/main/Java/ToolBar.java
@@ -17,15 +17,19 @@ public class ToolBar extends JPanel {
 
     void createButtons(){
         JButton exampleButton0 = new JButton("Example 0");
+        exampleButton0.setBackground(Color.WHITE);
         exampleButton0.setPreferredSize(new Dimension(150,50));
         add(exampleButton0);
         JButton exampleButton1 = new JButton("Another Example");
+        exampleButton1.setBackground(Color.WHITE);
         exampleButton1.setPreferredSize(new Dimension(150,50));
         add(exampleButton1);
         JButton exampleButton2 = new JButton("Exa");
+        exampleButton2.setBackground(Color.WHITE);
         exampleButton2.setPreferredSize(new Dimension(150,50));
         add(exampleButton2);
         JButton exampleButton3 = new JButton("Example Last");
+        exampleButton3.setBackground(Color.WHITE);
         exampleButton3.setPreferredSize(new Dimension(150,50));
         add(exampleButton3);
     }
