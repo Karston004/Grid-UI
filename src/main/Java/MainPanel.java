2606bf922dc96d7b87b5eeccb9e6b64807e43e9f
Smithj50 <j.smith47@lancaster.ac.uk>
2025-09-03 22:53:41 +0100

    chore: Added comments to MainPanel

--- src/main/Java/MainPanel.java
+++ src/main/Java/MainPanel.java
@@ -15,6 +15,8 @@ import javax.swing.BorderFactory;
 
 public class MainPanel extends JPanel {
     AbstractGridCanvas gridCanvas;
+
+    //Construtor
     MainPanel () {
         super(new GridBagLayout());
         setBorder(BorderFactory.createLineBorder(Color.RED));
@@ -22,12 +24,14 @@ public class MainPanel extends JPanel {
         addMainCanvas(1, 1, defaultColorMap());
         addToolBar(1, 0);
         
-        //Example grid
+        //Example grid - temp code
         int[][] exampleGrid = {{1,1,0,0},{1,0,0,1},{0,0,1,1},{0,1,1,1}};
         gridCanvas.SetGrid(exampleGrid);
 
     }
 
+    //Returns a color map for the mainCanvas to use
+    //only maps key 0 and 1
     HashMap<Integer, Color> defaultColorMap(){
         HashMap<Integer, Color> defaultColorMap = new HashMap<Integer,Color>();
         defaultColorMap.put(0, Color.GRAY);
@@ -35,6 +39,8 @@ public class MainPanel extends JPanel {
         return defaultColorMap;
     }
 
+    //Creates grid canvas and adds it to display
+    // gridx/gridy is x y postion on main panel's gridBagLayout
     void addMainCanvas(int gridx, int gridy, HashMap<Integer, Color> colorMap){
         gridCanvas = new HexGridCanvas(0, 0, colorMap);
         GridBagConstraints c = new GridBagConstraints();
@@ -46,6 +52,8 @@ public class MainPanel extends JPanel {
         add(gridCanvas, c);
     }
 
+    //Creates ToolBar
+    // gridx/gridy is x y postion on main panel's gridBagLayout
     void addToolBar(int gridx, int gridy){
         ToolBar toolBar = new ToolBar();
         GridBagConstraints c = new GridBagConstraints();
